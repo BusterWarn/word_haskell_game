d256353eed5d0c9ef1b4f7b2e6bcb819c768b192
Buster Hultgren Wärn <buster.hultgrenwarn@tietoevry.com>
2023-07-19 18:47:35 +0200

    Added diagonal searching

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,13 @@
 module Lib
-    ( grid
+    ( word_grid
     , languages
     , outputGrid
     , formatGrid
     , findWord
     , findWordInLine
     , findWords
+    , skew
+    , diagonalize
     ) where
 
 import Data.List (isInfixOf, transpose)
@@ -14,7 +16,9 @@ import Data.Maybe (catMaybes)
 type Grid = [String]
 
 outputGrid :: Grid -> IO ()
-outputGrid grid = putStrLn (formatGrid grid)
+outputGrid grid = do
+  putStrLn $ "length: " ++ show (length (formatGrid grid))
+  putStrLn $ formatGrid grid
 
 formatGrid :: Grid -> String
 formatGrid = unlines
@@ -23,7 +27,9 @@ gridToLines :: Grid -> [String]
 gridToLines grid =
   let horizontal = grid
       vertical = transpose grid
-      lines = horizontal ++ vertical
+      diagonal_horizontal = diagonalize grid
+      diagonal_vertical = diagonalize (map reverse horizontal)
+      lines = horizontal ++ vertical ++ diagonal_horizontal ++ diagonal_vertical
   in lines ++ (map reverse lines)
 
 findWord :: Grid -> String -> Maybe String
@@ -40,30 +46,41 @@ findWords grid words =
   let foundWords = map (findWord grid) words
   in catMaybes foundWords
 
-grid :: Grid
-grid = [ "__C________R___"
-       , "__SI________U__"
-       , "__HASKELL____B_"
-       , "__A__A_____S__Y"
-       , "__R___B___C____"
-       , "__PHP____H_____"
-       , "____S_LREP_____"
-       , "____I__M_Y__L__"
-       , "____L_E__T_O___"
-       , "_________HB____"
-       , "_________O_____"
-       , "________CN_____"
-       ]
+skew :: Grid -> Grid
+skew [] = []
+skew (l : ls) = do
+  l : skew (map indent ls)
+    where indent line = '_' : line
+
+diagonalize :: Grid -> Grid
+diagonalize = transpose . skew
+
+word_grid :: Grid
+word_grid =
+  [ "__C________R___"
+  , "__SI________U__"
+  , "__HASKELL____B_"
+  , "__A__A_____S__Y"
+  , "__R___B___C____"
+  , "__PHP____H_____"
+  , "____S_LREP_____"
+  , "____I__M_Y__L__"
+  , "____L_E__T_O___"
+  , "_________HB____"
+  , "_________O_____"
+  , "________CN_____"
+  ]
 
 languages :: [String]
-languages = [ "BASIC"
-            , "COBOL"
-            , "CSHARP"
-            , "HASKELL"
-            , "LISP"
-            , "PERL"
-            , "PHP"
-            , "PYTHON"
-            , "RUBY"
-            , "SCHEME"
-            ]
+languages =
+  [ "BASIC"
+  , "COBOL"
+  , "CSHARP"
+  , "HASKELL"
+  , "LISP"
+  , "PERL"
+  , "PHP"
+  , "PYTHON"
+  , "RUBY"
+  , "SCHEME"
+  ]
